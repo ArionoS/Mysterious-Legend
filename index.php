ec84a1a17603ef4aa1b1c05d6039fc4e83435b5b
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2022-06-09 22:36:35 +0700

    FIXING UI

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <?php
 require_once('includes/init.php');
-$judul_page = 'FINAL PROJECT SPK';
+$judul_page = 'Assesment Employee - TOPSIS & SAW';
 require_once('template-parts/header.php');
 ?>
 
-<html>
 
-<body>
-    <h1 style="text-align: center;">DASHBOARD PAGE</h1>
-</body>
 
-</html>
+<?php $user_role ?>
+
+<div id="main">
+    <h1>DASHBOARD PAGE</h1>
+</div>
+
+
+
+
+
 
 
 <?php
--- template-parts/header.php
+++ template-parts/header.php
@@ -1,78 +1,296 @@
 <!DOCTYPE html>
+<html lang="en">
 
 <head>
-	<meta http-equiv="x-ua-compatible" content="ie=edge" />
-	<meta charset="UTF-8" />
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
 	<title><?php
 			if (isset($judul_page)) {
 				echo $judul_page;
 			}
 			?></title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico" />
-	<link rel="stylesheet" href="stylesheets/style.css">
-	<script type="text/javascript" src="js/jquery-1.11.2.min.js"></script>
-	<script type="text/javascript" src="js/superfish.min.js"></script>
-	<script type="text/javascript" src="js/main.js"></script>
+	<link rel="icon" type="image/x-icon" href="stylesheets/new/assets/img/favicon.ico" />
+	<!-- BEGIN GLOBAL MANDATORY STYLES -->
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
+	<link href="stylesheets/new/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<link href="stylesheets/new/assets/css/plugins.css" rel="stylesheet" type="text/css" />
+	<!-- END GLOBAL MANDATORY STYLES -->
+
+	<!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM STYLES -->
+	<link href="stylesheets/new/plugins/maps/vector/jvector/jquery-jvectormap-2.0.3.css" rel="stylesheet" type="text/css" />
+	<link href="stylesheets/new/plugins/charts/chartist/chartist.css" rel="stylesheet" type="text/css">
+	<link href="stylesheets/new/assets/css/default-dashboard/style.css" rel="stylesheet" type="text/css" />
+	<!-- END PAGE LEVEL PLUGINS/CUSTOM STYLES -->
+
 </head>
 
-<body>
-	<div id="page">
+<body class="default-sidebar">
 
-		<header id="header">
-			<div class="container clearfix">
-				<div id="logo-wrap">
-					<a href="index.php">
+	<!--  BEGIN NAVBAR  -->
+	<header class="desktop-nav header navbar fixed-top">
+		<div class="nav-logo mr-5 ml-4 d-lg-inline-block d-none">
+			<a href="index.html" class=""> <img src="stylesheets/new/assets/img/logo-3.png" class="img-fluid" alt="logo"></a>
+		</div>
+		<ul class="navbar-nav flex-row mr-auto">
+			<li class="nav-item dropdown language-dropdown mr-5  d-lg-inline-block d-none">
+				<a href="javascript:void(0);" class="nav-link dropdown-toggle" id="language-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					<img src="stylesheets/new/assets/img/ca.png" alt=""> LOGO HERE
+				</a>
 
+			</li>
 
-						<h1 class="button">Penilaian Kinerja Pegawai</h1>
-					</a>
-				</div>
+		</ul>
 
-				<div id="header-content" class="clearfix">
-					<nav id="nav">
-						<ul class="sf-menu">
+		<ul class="navbar-nav flex-row ml-lg-auto">
+			<li class="nav-item mr-5 d-lg-block d-none">
+				<form class="form-inline form-inline search animated-search" role="search">
+					<i class="flaticon-search-1 d-lg-none d-block"></i>
+					<input type="text" class="form-control search-form-control  ml-lg-auto" placeholder="Search...">
+				</form>
+			</li>
+
+			<li class="nav-item dropdown user-profile-dropdown mr-5  d-lg-inline-block d-none">
+				<a href="javascript:void(0);" class="nav-link dropdown-toggle user" id="user-profile-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+
+					<div class="media">
+						<img src="stylesheets/new/assets/img/90x90.jpg" class="img-fluid mr-2" alt="admin-profile">
+						<div class="media-body align-self-center">
+				
 							<?php $user_role = get_role(); ?>
-							<?php if ($user_role == 'admin') : ?>
-								<li><a href="list-user.php">User</a>
-									<ul>
-										<li><a href="list-user.php">List User</a></li>
-										<li><a href="tambah-user.php">Tambah User</a></li>
+							<?php
+							if ($user_role == 'admin') { ?>
+								<h6 class="mb-1">ArJ Admin</h6>
+								<p class="mb-0">User Admin</p>
+
+							<?php } else if ($user_role == 'petugas') { ?>
+								<h6 class="mb-1">Petugas</h6>
+								<p class="mb-0">Administrator</p>
+
+							<?php } else { ?>
+								<h6 class="mb-1">Login First</h6>
+							<p class="mb-0">Click Here</p>
+							
+							<?php } ?>
+
+						</div>
+					</div>
+				</a>
+				<div class="dropdown-menu  position-absolute p-0" aria-labelledby="user-profile-dropdown">
+					<div class="dropdown-item d-flex justify-content-around">
+						<p class="mb-0 align-self-center">Your Account</p>
+						<div class="">
+							<i class="flaticon-star-outline"></i>
+						</div>
+					</div>
+					<div class="dropdown-divider"></div>
+					<a class="dropdown-item d-flex" href="#">
+						<i class="mr-3 flaticon-user-11"></i> <span class="align-self-center">Profile Setting</span>
+					</a>
+
+
+					<div class="dropdown-item dropdown-item-btn d-flex justify-content-around">
+						<?php if (isset($_SESSION['user_id'])) : ?>
+							<a href="logout.php"> <i class="mr-2 flaticon-power-off"></i> <span class="align-self-center">Logout</span></a>
+						<?php else : ?>
+							<a href="login.php"> <i class="mr-2 flaticon-power-off"></i> <span class="align-self-center">Login</span></a>
+						<?php endif; ?>
+
+
+					</div>
+				</div>
+			</li>
+
+
+
+
+
+			<li class="nav-item dropdown cs-toggle ml-3 mr-lg-4">
+				<?php if (isset($_SESSION['user_id'])) : ?>
+					<a href="logout.php"> <span class="icon flaticon-log-3"></span></a>
+				<?php else : ?>
+					<a href="login.php"><span class="icon flaticon-log-3"></span></a>
+				<?php endif; ?>
+
+
+			</li>
+		</ul>
+	</header>
+	<!--  END NAVBAR  -->
+
+	<!--  BEGIN MAIN CONTAINER  -->
+	<div class="main-container" id="container">
+		<div class="overlay"></div>
+		<div class="cs-overlay"></div>
+		<div class="search-overlay"></div>
+
+		<div class="topbar-nav header navbar fixed-top" role="banner">
+			<div id="dismiss" class="d-lg-none text-right"><i class="flaticon-cancel-12 mr-3"></i></div>
+			<nav id="topbar">
+				<ul class="list-unstyled menu-categories d-lg-flex justify-content-lg-around mb-0" id="topAccordion">
+
+
+
+					<?php $user_role = get_role(); ?>
+					<?php if ($user_role == 'admin') : ?>
+						<li class="menu">
+							<a href="#dashboardddd" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+								<div class="">
+									<i class="flaticon-computer-6"></i>
+									<span>User</span>
+								</div>
+
+								<div>
+									<i class="flaticon-down-arrow"></i>
+								</div>
+							</a>
+							<ul class="collapse submenu list-unstyled" id="dashboardddd" data-parent="#topAccordion">
+								<li>
+
+									<ul class="collapse list-unstyled sub-submenu show" id="dashboardddd">
+										<li class="active">
+											<a href="list-user.php"> List User </a>
+										</li>
+										<li>
+											<a href="tambah-user.php"> Tambah User </a>
+										</li>
+
 									</ul>
 								</li>
-								<li><a href="list-kriteria.php">Kriteria</a>
-									<ul>
-										<li><a href="list-kriteria.php">List Kriteria</a></li>
-										<li><a href="tambah-kriteria.php">Tambah Kriteria</a></li>
+							</ul>
+						</li>
+
+						<li class="menu">
+							<a href="#dashboarddd" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+								<div class="">
+									<i class="flaticon-computer-6"></i>
+									<span>Kriteria</span>
+								</div>
+
+								<div>
+									<i class="flaticon-down-arrow"></i>
+								</div>
+							</a>
+							<ul class="collapse submenu list-unstyled" id="dashboarddd" data-parent="#topAccordion">
+								<li>
+
+									<ul class="collapse list-unstyled sub-submenu show" id="dashboarddd">
+										<li class="active">
+											<a href="list-kriteria.php"> List Kriteria </a>
+										</li>
+										<li>
+											<a href="tambah-kriteria.php"> Tambah Kriteria </a>
+										</li>
+
+
 									</ul>
 								</li>
-							<?php endif; ?>
-							<?php if ($user_role == 'admin' || $user_role == 'petugas') : ?>
-								<li><a href="list-kambing.php">Pegawai</a>
-									<ul>
-										<li><a href="list-kambing.php">List Pegawai</a></li>
-										<li><a href="tambah-kambing.php">Tambah Pegawai</a></li>
+							</ul>
+						</li>
+					<?php endif; ?>
+
+					<?php if ($user_role == 'admin' || $user_role == 'petugas') : ?>
+						<li class="menu">
+							<a href="#dashboardd" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+								<div class="">
+									<i class="flaticon-computer-6"></i>
+									<span>Pegawai</span>
+								</div>
+
+								<div>
+									<i class="flaticon-down-arrow"></i>
+								</div>
+							</a>
+							<ul class="collapse submenu list-unstyled" id="dashboardd" data-parent="#topAccordion">
+								<li>
+
+									<ul class="collapse list-unstyled sub-submenu show" id="dashboardd">
+										<li class="active">
+											<a href="list-kambing.php"> List Pegawai </a>
+										</li>
+										<li>
+											<a href="tambah-kambing.php"> Tambah Pegawai </a>
+										</li>
+
 									</ul>
 								</li>
-							<?php endif; ?>
-							<li><a href="ranking-topsis.php">Ranking</a>
-								<ul>
-									<li><a href="ranking-topsis.php">Topsis</a></li>
-									<li><a href="ranking-saw.php">SAW</a></li>
+							</ul>
+						</li>
+					<?php endif; ?>
+
+					<li class="menu">
+						<a href="#dashboard" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">
+							<div class="">
+								<i class="flaticon-computer-6"></i>
+								<span>Ranking</span>
+							</div>
+
+							<div>
+								<i class="flaticon-down-arrow"></i>
+							</div>
+						</a>
+						<ul class="collapse submenu list-unstyled" id="dashboard" data-parent="#topAccordion">
+							<li>
+
+								<ul class="collapse list-unstyled sub-submenu show" id="dashboards">
+									<li class="active">
+										<a href="ranking-topsis.php"> Topsis </a>
+									</li>
+									<li>
+										<a href="ranking-saw.php"> SAW </a>
+									</li>
+
 								</ul>
 							</li>
 						</ul>
-					</nav>
+					</li>
 
-					<div id="header-right">
-						<?php if (isset($_SESSION['user_id'])) : ?>
-							<a href="logout.php" class="button">Log Out</a>
-						<?php else : ?>
-							<a href="login.php" class="button">Log In</a>
-						<?php endif; ?>
+
+
+
+				</ul>
+			</nav>
+		</div>
+
+
+		<!--  BEGIN CONTENT PART  -->
+		<div id="content" class="main-content">
+			<div class="container">
+				<div class="page-header">
+					<div class="page-title">
+						<h3>Dashboard</h3>
 					</div>
 				</div>
-			</div>
-		</header>
+				<div id="main">
 
-		<div id="main">
+
+
+
+					<!--  END CONTROL SIDEBAR  -->
+
+					<!-- BEGIN GLOBAL MANDATORY SCRIPTS -->
+					<script src="stylesheets/new/assets/js/libs/jquery-3.1.1.min.js"></script>
+					<script src="stylesheets/new/bootstrap/js/popper.min.js"></script>
+					<script src="stylesheets/new/bootstrap/js/bootstrap.min.js"></script>
+					<script src="stylesheets/new/plugins/scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
+					<script src="stylesheets/new/assets/js/app.js"></script>
+					<script>
+						$(document).ready(function() {
+							App.init();
+						});
+					</script>
+					<script src="stylesheets/new/assets/js/custom.js"></script>
+					<!-- END GLOBAL MANDATORY SCRIPTS -->
+
+					<!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
+					<script src="stylesheets/new/plugins/charts/chartist/chartist.js"></script>
+					<script src="stylesheets/new/plugins/maps/vector/jvector/jquery-jvectormap-2.0.3.min.js"></script>
+					<script src="stylesheets/new/plugins/maps/vector/jvector/worldmap_script/jquery-jvectormap-world-mill-en.js"></script>
+					<script src="stylesheets/new/plugins/calendar/pignose/moment.latest.min.js"></script>
+					<script src="stylesheets/new/plugins/calendar/pignose/pignose.calendar.js"></script>
+					<script src="stylesheets/new/plugins/progressbar/progressbar.min.js"></script>
+					<script src="stylesheets/new/assets/js/default-dashboard/default-custom.js"></script>
+					<!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS -->
+</body>
+
+</html>
